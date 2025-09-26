5ba7acee1237f06f0f3bff80fc8e275392ca39fb
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-09-26 21:56:24 +0800

    tests: add for login endpoint

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -3,22 +3,49 @@ import { app } from "../app.js";
 import "./setup.js";
 import prisma from "../db/prisma.js";
 
-describe("POST /api/auth/register", function () {
-  beforeEach(async () => {
+const USER = { username: "123", password: "123", email: "123@123.123" };
+
+describe("authRouter /api/auth", function () {
+  beforeAll(async () => {
     await prisma.user.deleteMany();
   });
 
-  it("registers a user", async function () {
+  it("POST ./register registers a user", async function () {
+    const user = USER;
     const response = await request(app)
       .post("/api/auth/register")
-      .send({ username: "123", password: "123", email: "123@123.123" })
+      .send(user)
       .set("Accept", "application/json")
       .expect("Content-Type", /json/)
       .expect(201);
 
-    expect(response.body.data).toBeDefined();
+    expect(response.body.data.username).toBe(user.username);
 
     const users = await prisma.user.findMany();
     expect(users.length).toBe(1);
+  });
+
+  it("POST ./login logs a valid user in", async function () {
+    const user = USER;
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send(user)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(response.body.data.username).toBe(user.username);
+  });
+
+  it("POST ./login returns 401 on wrong credentials", async function () {
+    const wrongCredentials = { username: "john", password: "321" };
+    const response = await request(app)
+      .post("/api/auth/login")
+      .send(wrongCredentials)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(401);
+
+    expect(response.body.error).toBeDefined();
   });
 });
