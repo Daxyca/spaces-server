bc2ccf59ec0b110375810e3330c60684b1a9c4f8
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-09-27 19:30:59 +0800

    fix: update profile queries to match new data model

--- db/profileQueries.js
+++ db/profileQueries.js
@@ -3,7 +3,7 @@ import prisma from "./prisma.js";
 export async function getUserProfile(userId) {
   return await prisma.profile.findFirst({
     where: {
-      userId,
+      id: userId,
     },
   });
 }
@@ -11,7 +11,7 @@ export async function getUserProfile(userId) {
 export async function createProfile(userId, profile = {}) {
   return await prisma.profile.create({
     data: {
-      userId: userId,
+      id: userId,
       displayName: profile.displayName,
       firstName: profile.firstName,
       lastName: profile.lastName,
@@ -26,7 +26,7 @@ export async function createProfile(userId, profile = {}) {
 export async function updateProfile(userId, profile = {}) {
   return await prisma.profile.update({
     where: {
-      userId,
+      id: userId,
     },
     data: {
       displayName: profile.displayName,
