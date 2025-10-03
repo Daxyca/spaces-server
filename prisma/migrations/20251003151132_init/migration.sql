c8d2f530da7e69995f38e75bd56d5faeb03a47eb
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-03 23:12:13 +0800

    chore: unify prisma migrations to one

--- prisma/migrations/20251003151132_init/migration.sql
+++ prisma/migrations/20251003151132_init/migration.sql
@@ -17,12 +17,25 @@ CREATE TABLE "public"."Session" (
 -- CreateTable
 CREATE TABLE "public"."User" (
     "id" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
+    "username" TEXT,
     "email" TEXT NOT NULL,
-    "passwordHash" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerUserId" TEXT,
+    "passwordHash" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -82,11 +95,19 @@ CREATE TABLE "public"."Feed" (
 );
 
 -- CreateTable
-CREATE TABLE "public"."_Likes" (
+CREATE TABLE "public"."_PostLikes" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL,
 
-    CONSTRAINT "_Likes_AB_pkey" PRIMARY KEY ("A","B")
+    CONSTRAINT "_PostLikes_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "public"."_CommentLikes" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_CommentLikes_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateTable
@@ -107,10 +128,22 @@ CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
 CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
 
 -- CreateIndex
-CREATE INDEX "_Likes_B_index" ON "public"."_Likes"("B");
+CREATE UNIQUE INDEX "Account_provider_providerUserId_key" ON "public"."Account"("provider", "providerUserId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Feed_authorId_name_key" ON "public"."Feed"("authorId", "name");
+
+-- CreateIndex
+CREATE INDEX "_PostLikes_B_index" ON "public"."_PostLikes"("B");
+
+-- CreateIndex
+CREATE INDEX "_CommentLikes_B_index" ON "public"."_CommentLikes"("B");
 
 -- CreateIndex
 CREATE INDEX "_FeedToProfile_B_index" ON "public"."_FeedToProfile"("B");
+
+-- AddForeignKey
+ALTER TABLE "public"."Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."Follow" ADD CONSTRAINT "Follow_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "public"."Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -134,10 +167,16 @@ ALTER TABLE "public"."Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY 
 ALTER TABLE "public"."Feed" ADD CONSTRAINT "Feed_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "public"."Profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."_Likes" ADD CONSTRAINT "_Likes_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."_PostLikes" ADD CONSTRAINT "_PostLikes_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."_Likes" ADD CONSTRAINT "_Likes_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."_PostLikes" ADD CONSTRAINT "_PostLikes_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."_CommentLikes" ADD CONSTRAINT "_CommentLikes_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."_CommentLikes" ADD CONSTRAINT "_CommentLikes_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."Profile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "public"."_FeedToProfile" ADD CONSTRAINT "_FeedToProfile_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Feed"("id") ON DELETE CASCADE ON UPDATE CASCADE;
