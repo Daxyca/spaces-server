b3f9d31e58b6f0999a83f0c62ef6c6100a55fa26
Daxyca <229032292+Daxyca@users.noreply.github.com>
2025-10-12 12:51:00 +0800

    refactor: proxy true on cookie

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: sessionStore,
+    proxy: true,
     cookie: {
       maxAge: 1 * 24 * 60 * 60 * 1000,
       httpOnly: true,
